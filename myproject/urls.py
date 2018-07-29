6b1ae012cd65b2676197e28932fe87f44d836465
avinash-tiwari <avikvvv@gmail.com>
2018-07-29 11:12:01 +0530

    different urls file created and facebook comments api integrated

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,13 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from post import views 
 from accounts import views as account_view
 urlpatterns = [
+    path('', include('post.urls')),
     path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
-    path('post/<int:pk>/',views.show_topics, name='show_topics'),
-    path('post/new/', views.post_topics, name='post_topics'),
     path('signup/',account_view.signup,name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout')
